275742a1ae65615e9683d81e3434ebf72908c4de
sunilpratap2911 <sunilpratapsingh29@gmail.com>
2024-12-21 12:13:34 +0530

    changed alert message for user

--- src/main.py
+++ src/main.py
@@ -22,9 +22,9 @@ def index():
             attempts += 1
 
             if guess < number_to_guess:
-                message = "Too low! Try again."
+                message = "Too low! Please Try again."
             elif guess > number_to_guess:
-                message = "Too high! Try again."
+                message = "Too high! Please Try again."
             else:
                 message = f"Congratulations! You guessed it in {attempts} attempts."
                 number_to_guess = None  # Reset the game
